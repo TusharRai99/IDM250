e333930f7555ec4a948cae6f4424b792327eba2f
TusharRai99 <72166628+TusharRai99@users.noreply.github.com>
2022-01-30 22:19:01 -0500

    Add files via upload

---
+++ header.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,600,700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=EB+Garamond:wght@600&display=swap" rel="stylesheet">
+    <title><?php wp_title(); ?>
+    </title>
+    <?php
+    // Link - https://developer.wordpress.org/reference/functions/wp_head/
+    // Plugins and WordPress core use this function to insert crucial elements into your document (e.g., scripts, styles, and meta tags).
+    // Always put wp_head() just before the closing tag of your theme
+    wp_head();
+    ?>
+
+    <section class="header">
+
+
+        <nav>
+            <a href="index.html"><img src="./images/logo.png"></a>
+            <div class="nav-links" id="navLinks">
+                <i class="fa fa-close" onclick="hideMenu()"></i>
+                <ul>
+
+                    <li><a href="#about">ABOUT</a></li>
+                    <li><a href="#services">PROJECTS</a></li>
+                    <li><a href="#whyus">BLOG</a></li>
+                    <li><a href="#contact">CONTACT</a></li>
+                </ul>
+            </div>
+            <i class="fa fa-bars" onclick="showMenu()"></i>
+        </nav>
+
+    </section>
+
+
+
+
+</head>
+
+<body>
+
+    <body>
+        <?php
+        // Registered this menu in functions.php
+        wp_nav_menu(['theme_location' => 'primary_menu']); ?>
